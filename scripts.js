7acdb690031fa7dae28e1ddf82ed27d492343cf1
qq1402629356 <1402629356@qq.com>
2020-06-20 21:05:28 +0800

    第三次提交

--- scripts.js
+++ scripts.js
@@ -4,31 +4,28 @@ let tasks = [];
 function renderEditor() {
     let inputEl = document.querySelector("#default-todo-panel .todo-editor > input");
 
-    //inputEl.onchange = (e) => {
-    //    console.log("text,", e.target.value);
-    //    console.log("input change:", e);
-   // };
+    let addTask = () => {
+        if (inputEl.value.length === 0) {
+            return;
+        }
+        let newTask = {
+            title: inputEl.value,
+            done: false,
+        };
 
-   let addTask = () => {
-    let newTask = {
-        title : inputEl.value,
-        done: false,
-   };
+        inputEl.value = "";
 
-   inputEl.value = "";
+        tasks.push(newTask);
 
-   tasks.push(newTask);
+        renderTaskItems();
+    };
 
-   console.log("tasks:", tasks);
-   renderTaskItems();
-   };
+    inputEl.onkeypress = (e) => {
 
-   inputEl.onkeypress = (e) =>{
-       
-       if (e.key==="Enter"){
-         addTask();  
-       }
-   };
+        if (e.key === "Enter") {
+            addTask();
+        }
+    };
 
     let addEl = document.querySelector("#default-todo-panel .todo-editor > button");
     addEl.onclick = (e) => {
@@ -39,31 +36,51 @@ function renderEditor() {
 function renderTaskItems() {
     console.log("render items");
     let itemsEl = document.querySelector("#default-todo-panel .todo-items");
-    
-    itemsEl.querySelectorAll("div").forEach((node)=>node.remove());
+
+    itemsEl.querySelectorAll("div").forEach((node) => node.remove());
 
     console.log(itemsEl);
-    
-    for(let i = 0; i < tasks.length; i++ )  {
+
+    for (let i = 0; i < tasks.length; i++) {
         let task = tasks[i];
         let itemEl = document.createElement("div");
+        itemEl.className = "task";
 
 
         let doneEl = document.createElement("input");
         doneEl.type = "checkbox";
+        doneEl.checked = task.done;
+        if (task.done) {
+            itemEl.classList.add("done");
+        } else {
+            itemEl.classList.remove("done");
+        }
+        doneEl.onchange = (e) => {
+            task.done = e.target.checked;
+            if (task.done) {
+                itemEl.classList.add("done");
+            } else {
+                itemEl.classList.remove("done");
+            }
+        }
         itemEl.append(doneEl);
 
         let titleEl = document.createElement("label");
         titleEl.innerText = task.title;
         itemEl.append(titleEl);
 
+        let ctrlbarEl = document.createElement("div");
+        ctrlbarEl.className = "ctrlbar";
+
         let cancelEl = document.createElement("button");
         cancelEl.innerText = "X";
-        cancelEl.onclick = () =>{
+        cancelEl.onclick = () => {
             tasks.splice(i, 1);
             renderTaskItems();
         };
-        itemEl.append(cancelEl);
+        ctrlbarEl.append(cancelEl);
+
+        itemEl.append(ctrlbarEl);
 
         itemsEl.append(itemEl);
     }
